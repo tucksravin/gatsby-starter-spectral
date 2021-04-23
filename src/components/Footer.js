55762aa49782383f313d9d0acf6aadc29436d16a
tucker <tucksravine1@gmail.com>
2021-04-23 13:09:18 -0700

    updated copy year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer id="footer">
       <div className="copyright">
-        <p>&copy; Tucker Lemos Creative, or Incidental Fox Productions 2020</p>
+        <p>&copy; Tucker Lemos Creative, or Incidental Fox Productions 2021</p>
         <p>
          Design forked from Spectral by <a href="http://html5up.net">HTML5 UP</a>
         </p>
